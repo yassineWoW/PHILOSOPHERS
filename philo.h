8c10d25e7fd21fda10c364e322d5ef5cb571c381
user007 <user007@42er.myguest.virtualbox.org>
2024-10-03 23:55:23 +0100

    utils_and_parsing

--- philo.h
+++ philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yimizare <yimizare@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user007 <user007@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 22:33:15 by yimizare          #+#    #+#             */
-/*   Updated: 2024/09/24 16:27:48 by yimizare         ###   ########.fr       */
+/*   Updated: 2024/10/03 23:25:09 by user007          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,10 @@
 # include <unistd.h>
 # include <stdbool.h>
 
+
+typedef	struct timeval	t_time;
+
+
 typedef struct s_info
 {
 	int				philos_num;
@@ -29,23 +33,31 @@ typedef struct s_info
 	size_t			tt_sleep;
 	size_t			tt_eat;
 	bool			deceased;
-	pthread_mutex_t	forks;
+	pthread_t		monitor;
+	
+	
 	
 	
 	
 
-	
+-	
 }	t_info;
 
 
 
 typedef struct s_philo
 {
-	
-
+	int				philo_id;
+	size_t			last_time_ate;
+	size_t			meals_ate;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
 	
 }	t_philo;
 
 void	ft_putstr_fd(char *s, int fd);
+size_t	ft_gettime(void);
+int		ft_usleep(long time);
+int		ft_atoi(char *in);
 
 #endif
--- philo_utils.c
+++ philo_utils.c
@@ -3,14 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   philo_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yimizare <yimizare@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user007 <user007@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 22:48:39 by yimizare          #+#    #+#             */
-/*   Updated: 2024/09/22 22:50:30 by yimizare         ###   ########.fr       */
+/*   Updated: 2024/10/03 23:22:09 by user007          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+size_t	ft_get_time(void)
+{
+	t_time	time;
+	
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * 100) + (time.tv_usec / 1000));	
+}
+
+int	ft_usleep(long time)
+{
+	long	start;
+
+	start = ft_get_time();
+	while ((ft_get_time - start) < (size_t)time)
+		usleep(500);
+	return (0);
+}
+
+int	ft_atoi(char *in)
+{
+	int		res;
+	int		i;
+
+	i = 0;
+	res = 0;
+	while (in[i] == 32 || (in[i] >= 9 && in[i] <= 13))
+		i++;
+	if (in[i] == '+' || in[i] == '-')
+	{
+		if (in[i] == '-')
+		{
+			ft_putstr("[ERROR] give me positive numbers ...\n");
+			return (-1);
+		}
+		i++;
+	}
+	while (in[i])
+	{
+		if (ft_util(&res, in, &i) == -1)
+			return (-1);
+		i++;
+	}
+	return (res);
+}
+
 
 void	ft_putstr_fd(char *s, int fd)
 {
